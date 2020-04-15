d4bd19d2eccd40ae55166dc920fb6c25ec16986c
Arthur Kuivanen <arthurkuivanen@Arthurs-iMac.local>
2020-04-14 21:10:10 -0400

    got the challenge to work. Some confusion about why one way worked vs another, but no complaints now that its running.

--- linden-treasure-hunt/src/data.js
+++ linden-treasure-hunt/src/data.js
@@ -1,35 +1,46 @@
 const maps = [{
+    id:1,
     title: "Rock Wall Hunt",
     stamp: "/images/treasure/earth.jpg",
     level:"easy",
     location: "neighborhood",
     details:"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
 },
-{    title: "Secret in the trees",
+{
+    id:2,
+    title: "Secret in the trees",
     stamp: "/images/treasure/ufo.jpg",
     level:"medium",
     location: "woods",
     details:"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
 },
-{    title: "On the Train Trail",
+{
+    id:3,
+    title: "On the Train Trail",
     stamp: "/images/treasure/rocket.jpg",
     level:"easy",
     location: "woods",
     details:"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
 },
-{    title: "Too Hard for Most",
+{
+    id:4,
+    title: "Too Hard for Most",
     stamp: "/images/treasure/sattelite.jpg",
     level:"hard",
     location: "woods",
     details:"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
 },
-{    title: "Howdy Neigbors!",
+{
+    id:5,
+    title: "Howdy Neigbors!",
     stamp: "/images/treasure/star.jpg",
     level:"easy",
     location: "neighborhood",
     details:"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
 },
-{    title: "Who's Out There?",
+{
+    id:6,
+    title: "Who's Out There?",
     stamp: "/images/treasure/sun.jpg",
     level:"medium",
     location: "neighborhood",
