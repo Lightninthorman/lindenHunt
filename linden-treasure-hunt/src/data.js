ab57b3e05448f0f2eeb10aa5b4300ae8645b4d41
Arthur Kuivanen <arthurkuivanen@Arthurs-iMac.local>
2020-04-27 14:04:51 -0400

    added another set of clues. All I have time for today it looks like. Josie is up already and Isaac never went down.

--- linden-treasure-hunt/src/data.js
+++ linden-treasure-hunt/src/data.js
@@ -26,16 +26,38 @@ const maps = [{
 },
 {
     id:2,
-    title: "Secret in the trees",
+    title: "Train Track Treasure",
     stamp: "/images/treasure/ufo.jpg",
-    level:"medium",
+    level:"easy",
     location: "woods",
-    details:"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    details:"Just into the woods and a safe place to watch the trains go by.",
+    clues:[
+        {
+            clue:"Start down Winslow Drive toward the end of the circle. Make your way to the start of the trail leading into the woods.",
+            image: "/images/clues/trailhead1.jpg",
+            clicked:false
+        },
+        {
+            clue:"As you enter the woods, walk towards the train tracks. There is a fork in the path. The path to the right goes down to the train tracks and your trail goes left on the hill along side of the train tracks. DO NOT GO DOWN TO THE TRAIN TRACKS. Take the path leading to the left.",
+            image: "/images/clues/traintrackfork.jpg",
+            clicked:false
+        },
+        {
+            clue:"Continue on the path until you find an old telephone pole.",
+            image: "/images/clues/telephonepole.jpg",
+            clicked:false
+        },
+        {
+            clue:"Search around the base of the pole for a strange rock covering the treasure.",
+            image: "/images/clues/telephonerock.jpg",
+            clicked:false
+        }
+    ],
     parentsNeeded:false
 },
 {
     id:3,
-    title: "On the Train Trail",
+    title: "Edge of the Woods",
     stamp: "/images/treasure/rocket.jpg",
     level:"easy",
     location: "woods",
@@ -44,7 +66,7 @@ const maps = [{
 },
 {
     id:4,
-    title: "Too Hard for Most",
+    title: "The Leaning Pine",
     stamp: "/images/treasure/sattelite.jpg",
     level:"hard",
     location: "woods",
@@ -53,7 +75,7 @@ const maps = [{
 },
 {
     id:5,
-    title: "Howdy Neigbors!",
+    title: "Log Hole",
     stamp: "/images/treasure/star.jpg",
     level:"easy",
     location: "neighborhood",
